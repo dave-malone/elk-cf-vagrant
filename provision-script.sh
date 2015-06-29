1e2456ce53b450649b8cf3a53ccb05fac14a3880
David Malone <dmalone@DavidMaloneMBP.home>
2015-06-29 13:21:26 -0400

    provision script needs to cp and not mv resource files

--- provision-script.sh
+++ provision-script.sh
@@ -10,16 +10,16 @@ wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | sudo apt-key
 echo 'deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main' | sudo tee /etc/apt/sources.list.d/elasticsearch.list
 sudo apt-get update
 sudo apt-get -y install elasticsearch=1.4.4
-sudo mv /vagrant/resources/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+sudo cp /vagrant/resources/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 sudo service elasticsearch restart
 sudo update-rc.d elasticsearch defaults 95 10
 
 cd ~; wget https://download.elasticsearch.org/kibana/kibana/kibana-4.0.1-linux-x64.tar.gz
 tar xvf kibana-*.tar.gz
-sudo mv /vagrant/resources/kibana.yml ~/kibana-4*/config/kibana.yml
+sudo cp /vagrant/resources/kibana.yml ~/kibana-4*/config/kibana.yml
 sudo mkdir -p /opt/kibana
 sudo cp -R ~/kibana-4*/* /opt/kibana/
-sudo mv /vagrant/resources/kibana4.sh /etc/init.d/kibana4
+sudo cp /vagrant/resources/kibana4.sh /etc/init.d/kibana4
 cd /etc/init.d
 sudo chmod +x /etc/init.d/kibana4
 sudo update-rc.d kibana4 defaults 96 9
@@ -28,5 +28,5 @@ sudo service kibana4 start
 echo 'deb http://packages.elasticsearch.org/logstash/1.5/debian stable main' | sudo tee /etc/apt/sources.list.d/logstash.list
 sudo apt-get update
 sudo apt-get install logstash
-sudo mv /vagrant/resources/syslog.conf /etc/logstash/conf.d/syslog.conf
+sudo cp /vagrant/resources/syslog.conf /etc/logstash/conf.d/syslog.conf
 sudo service logstash restart
